13c185c2523cefce49d2961fcfce9f2a0650bd57
Teemu Härkönen <teemu.harkonen@student.lut.fi>
2024-12-01 08:54:40 +0200

    Day 1

---
+++ day1/historian.py
@@ -0,0 +1,19 @@
+import history as history
+import numpy as np
+
+file = open("./day1/data.txt")
+lines = file.readlines()
+
+data = history.parse( lines )
+dataSorted = history.match( data )
+
+distance = history.computeDistance( dataSorted )
+similarityScore = history.computeSimilarityScore( dataSorted )
+
+print( distance )
+print( similarityScore )
+
+
+
+
+
